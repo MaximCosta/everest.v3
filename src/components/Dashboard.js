94a55d80970d38c49b36bfd8b7512aeb14063053
Maxim-Costa <mxmcosta@gmail.com>
2021-08-24 02:03:55 +0200

    graphql

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,67 @@
+import { gql, useQuery } from "@apollo/client";
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+
+const ELEC = gql`
+    query SearchProducts {
+        products(options: { sort: { name: ASC } }) {
+            totalItems
+            items {
+                id
+                name
+                description
+                collections {
+                    id
+                    name
+                }
+                assets {
+                    name
+                    source
+                    preview
+                }
+                variants {
+                    id
+                    name
+                    price
+                }
+            }
+        }
+    }
+`;
+
+function ExchangeRates() {
+    const { loading, error, data } = useQuery(ELEC);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return data.products.items.map((item) => (
+        <div key={item.name + "_" + item.id} style={{ width: 300, margin: 20 }}>
+            <br />
+            <br />
+            <br />
+            <img
+                src={item.assets[0].preview}
+                alt={item.assets[0].name}
+                height="100"
+            />
+            <div>
+                {item.collections.map((category) => category.name).join(" > ")}
+            </div>
+            <h2>{item.name}</h2>
+            <div>{item.description}</div>
+
+            <ul>
+                {item.variants.map((product) => (
+                    <li>
+                        {product.id} : {product.name} : {product.price}€
+                    </li>
+                ))}
+            </ul>
+        </div>
+    ));
+}
 
 export default function Dashboard() {
     const [error, setError] = useState("");
@@ -28,6 +89,10 @@ export default function Dashboard() {
                 </div>
             </div>
             <button onClick={handleLogout}>Logout</button>
+            <br />
+            <div style={{ display: "flex" }}>
+                <ExchangeRates />
+            </div>
         </>
     );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "http://20.83.162.231:3000/shop-api",
+    cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
